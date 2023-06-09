90c82ca1cf8328ca90b247582124de429930ca11
murdakCreations <thisisimportant@gmail.com>
2023-06-09 13:13:30 +0800

    can add 1 ingredient

--- client/src/components/AddData.js
+++ client/src/components/AddData.js
@@ -7,43 +7,87 @@ export class AddData extends Component {
         super()
         this.state = {
             recipeName: "",
+            ingredient: [],
+            ingredientName: "",
+            ingredientQuantity: 0,
+            ingredientUnit: "",
+            ingredientCut: ""
         }
     }
 
     // POST using Axios
     addNewRecipe = () => {
-        //console.log(this.state.recipeName)
-        Axios.post('http://localhost:8080/add-recipe', {recipeName: this.state.recipeName})
+        Axios.post('http://localhost:8080/add-recipe', {
+            recipeName: this.state.recipeName,
+            ingredient: this.state.ingredient
+        })
     }
 
-    handleChange = e => {
+    // always make a separate component for reading values onchange
+    handleChangeRecipeName = e => {
         const {value} = e.target
         this.setState({ recipeName: value })
     }
 
+    handleChangeIngredientQuantity = e => {
+        const {value} = e.target
+        this.setState({ ingredientQuantity: value })
+    }
+
+    handleChangeIngredientUnit = e => {
+        const {value} = e.target
+        this.setState({ ingredientUnit: value })
+    }
+
+    handleChangeIngredientName = e => {
+        const {value} = e.target
+        this.setState({ ingredientName: value })
+    }
+
+    handleChangeIngredientCut = e => {
+        const {value} = e.target
+        this.setState({ ingredientCut: value })
+    }
+
+    submitIngredient = () => {
+        const {ingredientUnit, ingredientQuantity, ingredientName, ingredientCut} = this.state
+        this.setState({
+            ingredient: [{
+                ingredientName,
+                ingredientQuantity,
+                ingredientUnit,
+                ingredientCut
+            }]
+        })
+    }
+
     render() {
-        const {recipeName} = this.state
+        const {recipeName, ingredientUnit, ingredientQuantity, ingredientName, ingredientCut} = this.state
         return (
             <div className="addData">
                 <div className="container">
                     <form onSubmit={this.addNewRecipe}>
-                        <input id="recipeName" name="recipeName" placeholder="Type Recipe Name Here" value={recipeName} onChange={this.handleChange}/>
-                        {/*<div className="addForm">
+                        <input id="recipeName" name="recipeName" placeholder="Type Recipe Name Here" value={recipeName} onChange={this.handleChangeRecipeName}/>
+                        <div className="addForm">
                             <h3>Ingredient/s:</h3>
-                            <button onClick={
-                                () =>
-                                document.getElementById("addIng").style.display = "block"
-                            }>+ Add Ingredient</button>
-                            <form action="" id="addIng" style={{display: "none"}}>
-                                <input type="text" placeholder="Enter Ingredient..."/>
-                                <input type="submit"/>
-                                <input type="button" onClick={
-                                () =>
-                                document.getElementById("addIng").style.display = "none"
-                                } value="close"/>
+                            <form action="" id="addIng">
+                                <label>Quantity:</label>
+                                <input type="number" name="ingredientQuantity" value={ingredientQuantity} onChange={this.handleChangeIngredientQuantity}/>
+                                <br/>
+                                <label>Unit:</label>
+                                <input type="text" name="ingredientUnit" value={ingredientUnit} onChange={this.handleChangeIngredientUnit}/>
+                                <br/>
+                                <label>Ingredient:</label>
+                                <input type="text" name="ingredientUnit" value={ingredientName} onChange={this.handleChangeIngredientName}/>
+                                <br/>
+                                <label>Cut: (Optional)</label>
+                                <input type="text" name="ingredientUnit" value={ingredientCut} onChange={this.handleChangeIngredientCut}/>
+                                <br/>
+                                <input type="button" value="add ingredient" onClick={this.submitIngredient}/>
+                                <input type="button" value="close"/>
                             </form>
                         </div>
-                        <div className="addForm">
+                        {/*<div className="addForm">
                             <h3>Procedure/s:</h3>
                             <button onClick={
                                 () =>
