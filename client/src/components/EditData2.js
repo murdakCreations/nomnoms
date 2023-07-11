a6c231649d6a04531bf4fe93caa75e6ffb8c6edd
murdakCreations <thisisimportant@gmail.com>
2023-07-11 17:04:23 +0800

    can update ingredients

--- client/src/components/EditData2.js
+++ client/src/components/EditData2.js
@@ -7,13 +7,24 @@ class EditData extends Component {
   constructor(props) {
     super(props);
     this.onChangeRecipeName = this.onChangeRecipeName.bind(this);
-    this.onChangeStudentName = this.onChangeStudentName.bind(this);
-    this.onChangeWeight = this.onChangeWeight.bind(this);
-    this.onChangeHeight = this.onChangeHeight.bind(this);
     this.getRecipeID = this.getRecipeID.bind(this);
-    this.updateStudent = this.updateStudent.bind(this);
+    this.updateRecipe = this.updateRecipe.bind(this);
     this.deleteRecipe = this.deleteRecipe.bind(this);
-    this.computeBMIVal = this.computeBMIVal.bind(this)
+    this.handleChangeIngredientQuantity = this.handleChangeIngredientQuantity.bind(this) 
+    this.handleChangeIngredientQuantity =     this.handleChangeIngredientQuantity.bind(this)
+    this.handleChangeIngredientUnit =     this.handleChangeIngredientUnit.bind(this)
+    this.handleChangeIngredientName =     this.handleChangeIngredientName.bind(this)
+    this.handleChangeIngredientCut =     this.handleChangeIngredientCut.bind(this)
+    this.displayIngForm =     this.displayIngForm.bind(this)
+    this.submitIngredient =     this.submitIngredient.bind(this)
+    this.hideIngForm =     this.hideIngForm.bind(this)
+    this.handleEditIng =     this.handleEditIng.bind(this)
+    this.handleChangeIngredientNameEdit =     this.handleChangeIngredientNameEdit.bind(this)
+    this.handleChangeIngredientUnitEdit =     this.handleChangeIngredientUnitEdit.bind(this)
+    this.handleChangeIngredientQuantityEdit =     this.handleChangeIngredientQuantityEdit.bind(this)
+    this.handleChangeIngredientCutEdit =     this.handleChangeIngredientCutEdit.bind(this)
+    this.saveOnPageEditIng =     this.saveOnPageEditIng.bind(this)
+    this.closeOnPageEditIng =     this.closeOnPageEditIng.bind(this)
 
     this.state = {
       recipeName: "",
@@ -24,7 +35,6 @@ class EditData extends Component {
       ingredientUnit: "",
       ingredientCut: "",
       currentIndexIng: 0,
-      recipe: [],
       paramID: ""
     };
   }
@@ -39,51 +49,6 @@ class EditData extends Component {
     this.setState({
       recipeName: value.toLowerCase()
     }, console.log(this.state.recipeName))
-    // this.setState(function(prevState) {
-    //   return {
-    //     currentStudent: {
-    //       ...prevState.currentStudent,
-    //       studentID: id
-    //     }
-    //   };
-    // });
-  }
-
-  onChangeStudentName(e) {
-    const name = e.target.value;
-    
-    this.setState(prevState => ({
-      currentStudent: {
-        ...prevState.currentStudent,
-        studentName: name
-      }
-    }));
-  }
-
-  onChangeWeight(e) {
-    const weight = e.target.value;
-    
-    this.setState(prevState => ({
-      currentStudent: {
-        ...prevState.currentStudent,
-        weight: weight
-      }
-    }),
-    () =>this.computeBMIVal()
-    );
-  }
-
-  onChangeHeight(e) {
-    const height = e.target.value;
-    
-    this.setState(prevState => ({
-      currentStudent: {
-        ...prevState.currentStudent,
-        height: height
-      }
-    }),
-    () =>this.computeBMIVal()
-    );
   }
 
   getRecipeID(id) {
@@ -91,7 +56,8 @@ class EditData extends Component {
       .then(response => {
         response.data.data.getRecipe.map((val,key)=>{
           this.setState({
-            recipeName: val.recipeName
+            recipeName: val.recipeName,
+            ingredient: val.ingredient
           });
         })
       })
@@ -104,10 +70,10 @@ class EditData extends Component {
     })
   }
 
-  updateStudent() {
+  updateRecipe() {
     Axios.put(`https://nomnoms-backend.vercel.app/update-recipe/${this.state.paramID}`, {
         recipeName: this.state.recipeName,
-        // ingredient: this.state.ingredient,
+        ingredient: this.state.ingredient,
         // procedure: this.state.procedure
     })
       .then(() => {
@@ -115,19 +81,6 @@ class EditData extends Component {
       }).catch(e => {
         console.log(e)
       })   
-    // StudentBMIDataService.update(
-    //   this.state.currentStudent.studentID,
-    //   this.state.currentStudent
-    // )
-    //   .then(response => {
-    //     console.log(response.data);
-    //     this.setState({
-    //       message: "The student data was updated successfully!"
-    //     });
-    //   })
-    //   .catch(e => {
-    //     console.log(e);
-    //   });
   }
 
   deleteRecipe() { 
@@ -139,80 +92,243 @@ class EditData extends Component {
       })   
   }
 
-    computeBMIVal() {
-      if(this.state.currentStudent.weight>0.0 && this.state.currentStudent.height>0.0){
-          let w = this.state.currentStudent.weight
-          let h = this.state.currentStudent.height
-          let comp = Number(w/h**2)
-          if(comp < 15){
-            this.setState(prevState => ({
-              currentStudent: {
-                ...prevState.currentStudent,
-                BMIVal: "Very severely underweight"
-              }
-            }));
+  handleChangeIngredientQuantity = e => {
+    const {value} = e.target
+    this.setState({ ingredientQuantity: value })
+  }
+
+  handleChangeIngredientUnit = e => {
+      const {value} = e.target
+      this.setState({ ingredientUnit: value })
+  }
+
+  handleChangeIngredientName = e => {
+      const {value} = e.target
+      this.setState({ ingredientName: value.toLowerCase() })
+  }
+
+  handleChangeIngredientCut = e => {
+      const {value} = e.target
+      this.setState({ ingredientCut: value })
+  }
+
+  displayIngForm = () => {
+      const {ingredientName,
+      ingredientQuantity,
+      ingredientUnit,
+      ingredientCut} = this.state
+      const mainContainer = document.getElementById("addIngForm")
+      
+      // Create sub container with id
+      const subContainer = document.createElement('div')
+      subContainer.id = 'addIngSubContainer'
+      mainContainer.appendChild(subContainer);
+      
+      const container = document.getElementById("addIngSubContainer")
+      container.innerHTML = ""
+      
+      // Create multiple input elements
+      var values = [ingredientName,
+          ingredientQuantity,
+          ingredientUnit,
+          ingredientCut];
+
+      var labels = ["Ingredient Name: ",
+          "Ingredient Quantity: ",
+          "Ingredient Unit: ",
+          "Ingredient Cut: "];
+
+      for(var i = 0; i < values.length; i += 1) {
+          var lbl = document.createElement("label")
+          lbl.innerHTML = labels[i]
+          container.appendChild(lbl);
+          
+          var div = document.createElement("input")
+          div.value = values[i]
+          if(i == 0) {
+              div.addEventListener('change', this.handleChangeIngredientName)
           }
-          if(comp >= 15 && comp <=16){
-            this.setState(prevState => ({
-              currentStudent: {
-                ...prevState.currentStudent,
-                BMIVal: "Severely underweight"
-              }
-            }));
+          if(i == 1) {
+              div.addEventListener('change', this.handleChangeIngredientQuantity)
+              div.type = 'number'
           }
-          if(comp > 16 && comp <= 18.5){
-            this.setState(prevState => ({
-              currentStudent: {
-                ...prevState.currentStudent,
-                BMIVal: "Underweight"
-              }
-            }));
+          if(i == 2) {
+              div.addEventListener('change', this.handleChangeIngredientUnit)
           }
-          if(comp > 18.5 && comp <= 25){
-            this.setState(prevState => ({
-              currentStudent: {
-                ...prevState.currentStudent,
-                BMIVal: "Normal"
-              }
-            }));
+          if(i == 3) {
+              div.addEventListener('change', this.handleChangeIngredientCut)
           }
-          if(comp > 25 && comp <= 30){
-            this.setState(prevState => ({
-              currentStudent: {
-                ...prevState.currentStudent,
-                BMIVal: "Overweight"
-              }
-            }));
-          }
-          if(comp > 30 && comp <= 35){
-            this.setState(prevState => ({
-              currentStudent: {
-                ...prevState.currentStudent,
-                BMIVal: "Moderately Overweight"
-              }
-            }));
-          }
-          if(comp > 35 && comp <= 40){
-            this.setState(prevState => ({
-              currentStudent: {
-                ...prevState.currentStudent,
-                BMIVal: "Severely Overweight"
-              }
-            }));
-          }
-          if(comp > 40){
-            this.setState(prevState => ({
-              currentStudent: {
-                ...prevState.currentStudent,
-                BMIVal: "Very severely obese"
-              }
-            }));
-          }
+          container.appendChild(div);
+
+          var br = document.createElement("br")
+          container.appendChild(br);
       }
+
+      // Set up the button
+      const btn = document.createElement('input')
+      btn.type = 'button'
+      btn.value = 'Add Ingredient'
+      btn.addEventListener('click', this.submitIngredient);
+      container.appendChild(btn);
+      const closeBtn = document.createElement('input')
+      closeBtn.type = 'button'
+      closeBtn.value = 'Close'
+      closeBtn.addEventListener('click', this.hideIngForm);
+      container.appendChild(closeBtn);
+
+      // hide add ing button
+      const addIngBtn = document.getElementById("displayIng")
+      addIngBtn.style.visibility = "hidden"
+  }
+
+  submitIngredient = () => {
+    const {ingredientUnit, ingredientQuantity, ingredientName, ingredientCut} = this.state
+    const array = {
+        ingredientName,
+        ingredientQuantity,
+        ingredientUnit,
+        ingredientCut
+    }
+    this.setState({
+        ingredient: [
+            ...this.state.ingredient,
+            array
+        ]
+    })
+  }
+
+  hideIngForm = () => {
+    const container = document.getElementById("addIngSubContainer")
+    container.innerHTML = ""
+
+    // unhide add ing button
+    const addIngBtn = document.getElementById("displayIng")
+    addIngBtn.style.visibility = "visible"
+  }
+
+  handleEditIng = (index) => {
+    console.log(index)
+    const {ingredientName,
+    ingredientQuantity,
+    ingredientUnit,
+    ingredientCut, ingredient} = this.state
+    this.setState({currentIndexIng: index})
+    const mainContainer = document.getElementById("insertHere")
+    
+    // Create sub container with id
+    const subContainer = document.createElement('div')
+    subContainer.id = 'subContainer'
+    mainContainer.appendChild(subContainer);
+    
+    const container = document.getElementById("subContainer")
+    container.innerHTML = ""
+    const addIngForm = document.getElementById("addIngSubContainer")
+    if(addIngForm){
+      addIngForm.innerHTML = ""
+    }
+    
+    const currentIng = ingredient[index]
+    // Create multiple input elements
+    var values = [currentIng.ingredientName,
+      currentIng.ingredientQuantity,
+      currentIng.ingredientUnit,
+      currentIng.ingredientCut];
+
+    var labels = ["Ingredient Name: ",
+        "Ingredient Quantity: ",
+        "Ingredient Unit: ",
+        "Ingredient Cut: "];
+
+    for(var i = 0; i < values.length; i += 1) {
+        var lbl = document.createElement("label")
+        lbl.innerHTML = labels[i]
+        container.appendChild(lbl);
+        
+        var div = document.createElement("input")
+        div.value = values[i]
+        
+        if(i == 0) {
+            div.addEventListener('change', this.handleChangeIngredientNameEdit)
+        }
+        if(i == 1) {
+            div.addEventListener('change', this.handleChangeIngredientQuantityEdit)
+            div.type = 'number'
+        }
+        if(i == 2) {
+            div.addEventListener('change', this.handleChangeIngredientUnitEdit)
+        }
+        if(i == 3) {
+            div.addEventListener('change', this.handleChangeIngredientCutEdit)
+        }
+        container.appendChild(div);
+
+        var br = document.createElement("br")
+        container.appendChild(br);
     }
 
+    // Set up the button
+    const btn = document.createElement('input')
+    btn.type = 'button'
+    btn.value = 'Save'
+    btn.addEventListener('click', this.saveOnPageEditIng);
+    container.appendChild(btn);
+    const closeBtn = document.createElement('input')
+    closeBtn.type = 'button'
+    closeBtn.value = 'Close'
+    closeBtn.addEventListener('click', this.closeOnPageEditIng);
+    container.appendChild(closeBtn);
+  }
+  
+  handleChangeIngredientNameEdit = e => {
+    const {value} = e.target
+    this.setState({ ingredientName: value.toLowerCase() })
+  }
+
+  handleChangeIngredientUnitEdit = e => {
+      const {value} = e.target
+      this.setState({ ingredientUnit: value })
+  }
+
+  handleChangeIngredientQuantityEdit = e => {
+      const {value} = e.target
+      this.setState({ ingredientQuantity: value })
+  }
+
+  handleChangeIngredientCutEdit = e => {
+      const {value} = e.target
+      this.setState({ ingredientCut: value })
+      console.log(value)
+  }
+
+  saveOnPageEditIng = () => {
+    const {ingredientUnit, ingredientQuantity, ingredientName, ingredientCut, ingredient, currentIndexIng} = this.state
+
+      const array = {
+          ingredientName,
+          ingredientQuantity,
+          ingredientUnit,
+          ingredientCut
+      }
+
+      ingredient[currentIndexIng] = array
+      
+      this.setState({
+          ingredient
+      })
+      
+  }
+
+  closeOnPageEditIng = () => {
+      const container = document.getElementById("subContainer")
+      container.innerHTML = ""
+
+      // unhide add ing button
+      const addIngBtn = document.getElementById("displayIng")
+      addIngBtn.style.visibility = "visible"
+  }
+
   render() {
-    const { recipeName } = this.state;
+    const { recipeName, ingredient } = this.state;
 
     return (
       <div>
@@ -230,17 +346,25 @@ class EditData extends Component {
                     onChange={this.onChangeRecipeName}
                   />
               </div>
-              {/* <div className="form-group">
-                <label htmlFor="studentName">Student Name</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="studentName"
-                  value={currentStudent.studentName}
-                  onChange={this.onChangeStudentName}
-                />
+              {
+                ingredient.map((val, key) => {
+                  return <div className="form-group">
+                  <input id={key} value={val.ingredientQuantity +" "+ val.ingredientUnit+
+                  " " + val.ingredientName + " " + val.ingredientCut} disabled/>
+                  <input type="button" value="Edit" onClick={() => this.handleEditIng(key)}/>
+                  <input type="button" value="Delete" onClick={() => this.handleDelIng(key)}/>
+                </div>
+                })
+              }
+              <div id="insertHere">
+                                    
               </div>
-              <div className="form-group">
+              <input type="button" id="displayIng" value="+Add Ingredient" onClick={this.displayIngForm}/>
+              <div id="addIngForm">
+                  
+              </div>
+              
+              {/* <div className="form-group">
                 <label htmlFor="weight">Weight</label>
                 <input
                   type="text"
@@ -284,7 +408,7 @@ class EditData extends Component {
               type="submit"
               id="updateBtn"
               className="badge badge-success"
-              onClick={this.updateStudent}
+              onClick={this.updateRecipe}
             >
               Update
             </button>
